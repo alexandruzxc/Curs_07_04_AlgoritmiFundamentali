34f502242b8334d175f7531271f7d3c76660a459
Alexandru Ifrim <ifrim.alexandru0@gmail.com>
2022-04-08 09:33:42 +0300

    AddNewFilters

--- Curs_07_04_AlgoritmiFundamentali/Form1.Designer.cs
+++ Curs_07_04_AlgoritmiFundamentali/Form1.Designer.cs
@@ -40,6 +40,16 @@ namespace Curs_07_04_AlgoritmiFundamentali
             this.btnContrast = new System.Windows.Forms.Button();
             this.btnZoom = new System.Windows.Forms.Button();
             this.btnZoomV1 = new System.Windows.Forms.Button();
+            this.FilterBanalExample = new System.Windows.Forms.Button();
+            this.btnSepia = new System.Windows.Forms.Button();
+            this.btnComplement = new System.Windows.Forms.Button();
+            this.BtnCyan = new System.Windows.Forms.Button();
+            this.myFilter = new System.Windows.Forms.Button();
+            this.myFilterV2 = new System.Windows.Forms.Button();
+            this.myFilterV3 = new System.Windows.Forms.Button();
+            this.btnMax = new System.Windows.Forms.Button();
+            this.btnFirstGradiant = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
@@ -65,7 +75,7 @@ namespace Curs_07_04_AlgoritmiFundamentali
             this.btnDirect.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.btnDirect.Location = new System.Drawing.Point(12, 422);
             this.btnDirect.Name = "btnDirect";
-            this.btnDirect.Size = new System.Drawing.Size(110, 56);
+            this.btnDirect.Size = new System.Drawing.Size(46, 56);
             this.btnDirect.TabIndex = 2;
             this.btnDirect.Text = "btnDirect";
             this.btnDirect.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -74,9 +84,9 @@ namespace Curs_07_04_AlgoritmiFundamentali
             // 
             // btnGrayScalle
             // 
-            this.btnGrayScalle.Location = new System.Drawing.Point(141, 422);
+            this.btnGrayScalle.Location = new System.Drawing.Point(82, 417);
             this.btnGrayScalle.Name = "btnGrayScalle";
-            this.btnGrayScalle.Size = new System.Drawing.Size(112, 56);
+            this.btnGrayScalle.Size = new System.Drawing.Size(49, 56);
             this.btnGrayScalle.TabIndex = 3;
             this.btnGrayScalle.Text = "btnGrayScalle";
             this.btnGrayScalle.UseVisualStyleBackColor = true;
@@ -84,9 +94,9 @@ namespace Curs_07_04_AlgoritmiFundamentali
             // 
             // btnBlackAndWhite
             // 
-            this.btnBlackAndWhite.Location = new System.Drawing.Point(274, 422);
+            this.btnBlackAndWhite.Location = new System.Drawing.Point(154, 412);
             this.btnBlackAndWhite.Name = "btnBlackAndWhite";
-            this.btnBlackAndWhite.Size = new System.Drawing.Size(112, 56);
+            this.btnBlackAndWhite.Size = new System.Drawing.Size(40, 56);
             this.btnBlackAndWhite.TabIndex = 4;
             this.btnBlackAndWhite.Text = "btnBlackAndWhite";
             this.btnBlackAndWhite.UseVisualStyleBackColor = true;
@@ -95,9 +105,9 @@ namespace Curs_07_04_AlgoritmiFundamentali
             // btnUnknowName
             // 
             this.btnUnknowName.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
-            this.btnUnknowName.Location = new System.Drawing.Point(404, 422);
+            this.btnUnknowName.Location = new System.Drawing.Point(211, 412);
             this.btnUnknowName.Name = "btnUnknowName";
-            this.btnUnknowName.Size = new System.Drawing.Size(110, 56);
+            this.btnUnknowName.Size = new System.Drawing.Size(42, 56);
             this.btnUnknowName.TabIndex = 5;
             this.btnUnknowName.Text = "btnUnknowName";
             this.btnUnknowName.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -107,9 +117,9 @@ namespace Curs_07_04_AlgoritmiFundamentali
             // btnUnknowNameV2
             // 
             this.btnUnknowNameV2.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
-            this.btnUnknowNameV2.Location = new System.Drawing.Point(535, 422);
+            this.btnUnknowNameV2.Location = new System.Drawing.Point(268, 417);
             this.btnUnknowNameV2.Name = "btnUnknowNameV2";
-            this.btnUnknowNameV2.Size = new System.Drawing.Size(110, 56);
+            this.btnUnknowNameV2.Size = new System.Drawing.Size(32, 56);
             this.btnUnknowNameV2.TabIndex = 6;
             this.btnUnknowNameV2.Text = "btnUnknowNameV2";
             this.btnUnknowNameV2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -119,9 +129,9 @@ namespace Curs_07_04_AlgoritmiFundamentali
             // btnDarkness
             // 
             this.btnDarkness.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
-            this.btnDarkness.Location = new System.Drawing.Point(668, 422);
+            this.btnDarkness.Location = new System.Drawing.Point(334, 412);
             this.btnDarkness.Name = "btnDarkness";
-            this.btnDarkness.Size = new System.Drawing.Size(110, 56);
+            this.btnDarkness.Size = new System.Drawing.Size(38, 56);
             this.btnDarkness.TabIndex = 7;
             this.btnDarkness.Text = "btnDarkness";
             this.btnDarkness.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -131,9 +141,9 @@ namespace Curs_07_04_AlgoritmiFundamentali
             // btnContrast
             // 
             this.btnContrast.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
-            this.btnContrast.Location = new System.Drawing.Point(803, 422);
+            this.btnContrast.Location = new System.Drawing.Point(403, 412);
             this.btnContrast.Name = "btnContrast";
-            this.btnContrast.Size = new System.Drawing.Size(110, 56);
+            this.btnContrast.Size = new System.Drawing.Size(50, 56);
             this.btnContrast.TabIndex = 8;
             this.btnContrast.Text = "btnContrast";
             this.btnContrast.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -143,9 +153,9 @@ namespace Curs_07_04_AlgoritmiFundamentali
             // btnZoom
             // 
             this.btnZoom.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
-            this.btnZoom.Location = new System.Drawing.Point(968, 422);
+            this.btnZoom.Location = new System.Drawing.Point(482, 412);
             this.btnZoom.Name = "btnZoom";
-            this.btnZoom.Size = new System.Drawing.Size(110, 56);
+            this.btnZoom.Size = new System.Drawing.Size(43, 56);
             this.btnZoom.TabIndex = 9;
             this.btnZoom.Text = "btnZoom";
             this.btnZoom.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -154,19 +164,132 @@ namespace Curs_07_04_AlgoritmiFundamentali
             // 
             // btnZoomV1
             // 
-            this.btnZoomV1.Location = new System.Drawing.Point(1140, 422);
+            this.btnZoomV1.Location = new System.Drawing.Point(550, 412);
             this.btnZoomV1.Name = "btnZoomV1";
-            this.btnZoomV1.Size = new System.Drawing.Size(105, 56);
+            this.btnZoomV1.Size = new System.Drawing.Size(35, 56);
             this.btnZoomV1.TabIndex = 10;
             this.btnZoomV1.Text = "btnZoomV1";
             this.btnZoomV1.UseVisualStyleBackColor = true;
             this.btnZoomV1.Click += new System.EventHandler(this.btnZoomV1_Click);
+            // 
+            // FilterBanalExample
+            // 
+            this.FilterBanalExample.Location = new System.Drawing.Point(568, 131);
+            this.FilterBanalExample.Name = "FilterBanalExample";
+            this.FilterBanalExample.Size = new System.Drawing.Size(106, 46);
+            this.FilterBanalExample.TabIndex = 11;
+            this.FilterBanalExample.Text = "FilterBanalExample";
+            this.FilterBanalExample.UseVisualStyleBackColor = true;
+            this.FilterBanalExample.Click += new System.EventHandler(this.FilterBanalExample_Click);
+            // 
+            // btnSepia
+            // 
+            this.btnSepia.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnSepia.Location = new System.Drawing.Point(565, 195);
+            this.btnSepia.Name = "btnSepia";
+            this.btnSepia.Size = new System.Drawing.Size(109, 50);
+            this.btnSepia.TabIndex = 12;
+            this.btnSepia.Text = "btnSepia";
+            this.btnSepia.UseVisualStyleBackColor = false;
+            this.btnSepia.Click += new System.EventHandler(this.myVersion_Click);
+            // 
+            // btnComplement
+            // 
+            this.btnComplement.Location = new System.Drawing.Point(568, 264);
+            this.btnComplement.Name = "btnComplement";
+            this.btnComplement.Size = new System.Drawing.Size(99, 42);
+            this.btnComplement.TabIndex = 13;
+            this.btnComplement.Text = "btnComplement";
+            this.btnComplement.UseVisualStyleBackColor = true;
+            this.btnComplement.Click += new System.EventHandler(this.btnComplement_Click);
+            // 
+            // BtnCyan
+            // 
+            this.BtnCyan.Location = new System.Drawing.Point(565, 68);
+            this.BtnCyan.Name = "BtnCyan";
+            this.BtnCyan.Size = new System.Drawing.Size(109, 46);
+            this.BtnCyan.TabIndex = 14;
+            this.BtnCyan.Text = "BtnCyan";
+            this.BtnCyan.UseVisualStyleBackColor = true;
+            this.BtnCyan.Click += new System.EventHandler(this.BtnCyan_Click);
+            // 
+            // myFilter
+            // 
+            this.myFilter.Location = new System.Drawing.Point(565, 324);
+            this.myFilter.Name = "myFilter";
+            this.myFilter.Size = new System.Drawing.Size(102, 34);
+            this.myFilter.TabIndex = 15;
+            this.myFilter.Text = "myFilter";
+            this.myFilter.UseVisualStyleBackColor = true;
+            this.myFilter.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // myFilterV2
+            // 
+            this.myFilterV2.Location = new System.Drawing.Point(565, 13);
+            this.myFilterV2.Name = "myFilterV2";
+            this.myFilterV2.Size = new System.Drawing.Size(109, 39);
+            this.myFilterV2.TabIndex = 16;
+            this.myFilterV2.Text = "myFilterV2";
+            this.myFilterV2.UseMnemonic = false;
+            this.myFilterV2.UseVisualStyleBackColor = true;
+            this.myFilterV2.Click += new System.EventHandler(this.myFilterV2_Click);
+            // 
+            // myFilterV3
+            // 
+            this.myFilterV3.Location = new System.Drawing.Point(568, 378);
+            this.myFilterV3.Name = "myFilterV3";
+            this.myFilterV3.Size = new System.Drawing.Size(106, 29);
+            this.myFilterV3.TabIndex = 17;
+            this.myFilterV3.Text = "myFilterV3";
+            this.myFilterV3.UseVisualStyleBackColor = true;
+            this.myFilterV3.Click += new System.EventHandler(this.myFilterV3_Click);
+            // 
+            // btnMax
+            // 
+            this.btnMax.Location = new System.Drawing.Point(599, 412);
+            this.btnMax.Name = "btnMax";
+            this.btnMax.Size = new System.Drawing.Size(49, 46);
+            this.btnMax.TabIndex = 18;
+            this.btnMax.Text = "btnMax";
+            this.btnMax.UseVisualStyleBackColor = true;
+            this.btnMax.Click += new System.EventHandler(this.btnMax_Click);
+            // 
+            // btnFirstGradiant
+            // 
+            this.btnFirstGradiant.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.btnFirstGradiant.Location = new System.Drawing.Point(692, 402);
+            this.btnFirstGradiant.Name = "btnFirstGradiant";
+            this.btnFirstGradiant.Size = new System.Drawing.Size(85, 56);
+            this.btnFirstGradiant.TabIndex = 19;
+            this.btnFirstGradiant.Text = "btnFirstGradiant";
+            this.btnFirstGradiant.UseVisualStyleBackColor = true;
+            this.btnFirstGradiant.Click += new System.EventHandler(this.btnFirstGradiant_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(821, 402);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(51, 56);
+            this.button1.TabIndex = 20;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1369, 520);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnFirstGradiant);
+            this.Controls.Add(this.btnMax);
+            this.Controls.Add(this.myFilterV3);
+            this.Controls.Add(this.myFilterV2);
+            this.Controls.Add(this.myFilter);
+            this.Controls.Add(this.BtnCyan);
+            this.Controls.Add(this.btnComplement);
+            this.Controls.Add(this.btnSepia);
+            this.Controls.Add(this.FilterBanalExample);
             this.Controls.Add(this.btnZoomV1);
             this.Controls.Add(this.btnZoom);
             this.Controls.Add(this.btnContrast);
@@ -200,6 +323,16 @@ namespace Curs_07_04_AlgoritmiFundamentali
         private System.Windows.Forms.Button btnContrast;
         private System.Windows.Forms.Button btnZoom;
         private System.Windows.Forms.Button btnZoomV1;
+        private System.Windows.Forms.Button FilterBanalExample;
+        private System.Windows.Forms.Button btnSepia;
+        private System.Windows.Forms.Button btnComplement;
+        private System.Windows.Forms.Button BtnCyan;
+        private System.Windows.Forms.Button myFilter;
+        private System.Windows.Forms.Button myFilterV2;
+        private System.Windows.Forms.Button myFilterV3;
+        private System.Windows.Forms.Button btnMax;
+        private System.Windows.Forms.Button btnFirstGradiant;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Curs_07_04_AlgoritmiFundamentali/Form1.cs
+++ Curs_07_04_AlgoritmiFundamentali/Form1.cs
@@ -285,5 +285,238 @@ namespace Curs_07_04_AlgoritmiFundamentali
             pictureBox2.Image = destination;
 
         }
+
+        private void FilterBanalExample_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < source.Width; i++)
+            {
+                for (int j = 0; j < source.Height; j++)
+                {
+                    Color Ts = source.GetPixel(i, j);
+                    Color Td;
+                    int r = Ts.R;
+                    int g = 55;
+                    int b = 55;
+                    Td = Color.FromArgb(r, g, b);
+                    destination.SetPixel(i, j, Td);
+                }
+            }
+            pictureBox2.Image = destination;
+        }
+
+        private void myVersion_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < source.Width; i++)
+            {
+                for (int j = 0; j < source.Height; j++)
+                {
+                    Color Ts = source.GetPixel(i, j);
+                    Color Td;
+                    int a = Ts.A;
+                    int r = Ts.R;
+                    int g = Ts.G;
+                    int b = Ts.B;
+                    int tr = (int)(0.393 * r + 0.769 * g + 0.189 * b);
+                    int tg = (int)(0.349 * r + 0.686 * g + 0.168 * b);
+                    int tb = (int)(0.272 * r + 0.534 * g + 0.131 * b);
+                    if (tr > 255)
+                    {
+                        r = 255;
+                    }
+                    else
+                    {
+                        r = tr;
+                    }
+
+                    if (tg > 255)
+                    {
+                        g = 255;
+                    }
+                    else
+                    {
+                        g = tg;
+                    }
+
+                    if (tb > 255)
+                    {
+                        b = 255;
+                    }
+                    else
+                    {
+                        b = tb;
+                    }
+                    //bmp.SetPixel(x, y, Color.FromArgb(a, r, g, b));
+                    Td = Color.FromArgb(a, r, g, b);
+                    destination.SetPixel(i, j, Td);
+                }
+            }
+            pictureBox2.Image = destination;
+        }
+
+        private void btnComplement_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < source.Width; i++)
+            {
+                for (int j = 0; j < source.Height; j++)
+                {
+                    Color Ts = source.GetPixel(i, j);
+                    Color Td;
+                    int r = 255 - Ts.R;
+                    int g = 255 - Ts.G;
+                    int b = 255 - Ts.B;
+                    Td = Color.FromArgb(r, g, b);
+                    destination.SetPixel(i, j, Td);
+                    Color t = Color.Cyan;
+                }
+            }
+            pictureBox2.Image = destination;
+        }
+
+        private void BtnCyan_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < source.Width; i++)
+            {
+                for (int j = 0; j < source.Height; j++)
+                {
+                    Color Ts = source.GetPixel(i, j);
+                    Color Td;
+                    int r = 0;
+                    int g = 0;
+                    int b = Ts.B;
+                    Td = Color.FromArgb(r, g, b);
+                    destination.SetPixel(i, j, Td);
+                    Color t = Color.Cyan;
+                }
+            }
+            pictureBox2.Image = destination;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < source.Width; i++)
+            {
+                for (int j = 0; j < source.Height; j++)
+                {
+                    Color Ts = source.GetPixel(i, j);
+                    Color Td;
+                    int r = Ts.R;
+                    int g = Ts.G;
+                    int b = Ts.B;
+                    if (r > 60 && r < 160)
+                        Td = Color.FromArgb(r, 0, 0);
+                    else
+                        Td = Color.FromArgb(255, 255, 255);
+                    destination.SetPixel(i, j, Td);
+                }
+            }
+            pictureBox2.Image = destination;
+        }
+
+        private void myFilterV2_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < source.Width; i++)
+            {
+                for (int j = 0; j < source.Height; j++)
+                {
+                    Color Ts = source.GetPixel(i, j);
+                    Color Td;
+                    int r = Ts.R;
+                    int g = Ts.G;
+                    int b = Ts.B;
+                    if (g > 60 && g < 160)
+                        Td = Color.FromArgb(0, g, 0);
+                    else
+                        Td = Color.FromArgb(255, 255, 0);
+                    destination.SetPixel(i, j, Td);
+                }
+            }
+            pictureBox2.Image = destination;
+        }
+
+        private void myFilterV3_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < source.Width; i++)
+            {
+                for (int j = 0; j < source.Height; j++)
+                {
+                    Color Ts = source.GetPixel(i, j);
+                    Color Td;
+                    int r = Ts.R;
+                    int g = Ts.G;
+                    int b = Ts.B;
+                    if (b > 60 && b < 160)
+                        Td = Color.FromArgb(0, 0, b);
+                    else
+                        Td = Color.FromArgb(255, 255, 255);
+                    destination.SetPixel(i, j, Td);
+                }
+            }
+            pictureBox2.Image = destination;
+        }
+
+        private void btnMax_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < source.Width; i++)
+            {
+                for (int j = 0; j < source.Height; j++)
+                {
+                    Color Ts = source.GetPixel(i, j);
+                    Color Td = Color.Black;
+                    int r = Ts.R;
+                    int g = Ts.G;
+                    int b = Ts.B;
+                   if (r >= g && r >= b)
+                        Td = Color.FromArgb(200, 0, 0);
+                   if (g >= b && g >= r)
+                        Td = Color.FromArgb(0, 200, 0);
+                    if (b >= r && b >= g)
+                        Td = Color.FromArgb(255, 200, 0);
+                    destination.SetPixel(i, j, Td);
+                }
+            }
+            pictureBox2.Image = destination;
+        }
+
+        private void btnFirstGradiant_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < source.Width; i++)
+            {
+                for (int j = 0; j < source.Height; j++)
+                {
+                    Color Ts = source.GetPixel(i, j);
+                    Color Td;
+                    int r = min((Ts.R + j), 255);
+                    int g = min((Ts.G + j), 255);
+                    int b = min((Ts.B + j), 255);
+                    Td = Color.FromArgb(r, g, b);
+                    destination.SetPixel(i, j, Td);
+                }
+            }
+            pictureBox2.Image = destination;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            int k = 5;
+            Color bs = Color.Black;
+            for (int i = 0; i < source.Width; i+= k)
+            {
+                for (int j = 0; j < source.Height; j += k)
+                {
+                    Color Ts = source.GetPixel(i, j);
+                    Color Td;
+                    for (int l = 0; l < k; l++)
+                    {
+                        for (int m = 0; m < k; m++)
+                        {
+                            destination.SetPixel((i + l) % source.Width, (j + m) % source.Height, Ts);
+                        }
+                    }
+                    
+                   
+                }
+            }
+            pictureBox2.Image = destination;
+        }
     }
 }
